ee8d91eb5414add97eb67fdb9c92f861093b1137
Emanueli Cristina <cristinagodoy89br@gmail.com>
2026-02-26 13:40:55 +0000

    generador carta aleatoria

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+   const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const palos = ["♥", "♦", "♠", "♣"];   
+
+function generarCarta() {
+  const numeroRandom = numeros[Math.floor(Math.random() * numeros.length)];
+  const paloRandom = palos[Math.floor(Math.random() * palos.length)];
+
+  document.querySelector(".numero").textContent = numeroRandom;
+  document.querySelectorAll(".palo").forEach(p => p.textContent = paloRandom);
+
+  
+  const color = (paloRandom === "♠" || paloRandom === "♣") ? "black" : "red";
+  document.querySelectorAll(".palo").forEach(p => p.style.color = color);
+}
+generarCarta();
+
+
+}
